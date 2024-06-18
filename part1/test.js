fddc9d766de2748eab2c49136f2fbd00ada7d112
NihushaganS <s22010005@ousl.lk>
2024-06-18 16:42:42 +0530

    pra

--- part1/test.js
+++ part1/test.js
@@ -3,3 +3,19 @@ const vari = 25
 
 console.log(number,vari)
 
+const arr = [1,2,3,4]
+console.log(arr.length) // measure legnth of array
+console.log(arr[2]) // index if each element
+
+arr.push(1000) // add new element last in the array
+console.log(arr)
+
+arr.forEach(value => { 
+  console.log(value) // access each element seperately
+})
+
+arr1 = arr.concat(12) // copy array and create new element
+console.log(arr1)
+console.log(arr)
+
+
